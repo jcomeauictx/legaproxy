76a81656d3907259b62c5d385ad394318d193d88
John Comeau <jc@unternet.net>
2024-01-30 21:11:53 -0800

    will now save pages from different user-agents into different paths

--- filter.py
+++ filter.py
@@ -3,12 +3,16 @@
 Using example internet-in-mirror.py from
 https://docs.mitmproxy.org/stable/addons-examples/
 '''
-import os, logging  # pylint: disable=multiple-imports
-from mitmproxy import http
+import os, logging, base64, hashlib  # pylint: disable=multiple-imports
+try:
+    from mitmproxy import http
+except (ImportError, ModuleNotFoundError):  # for doctests
+    http = type('', (), {'HTTPFlow': None})  # pylint: disable=invalid-name
 
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.WARNING)
 HOSTSUFFIX = 'redwoodcu.org'
 FILES = os.path.join('storage', 'files')
+# iphone6 (iOS 12.5.7) user-agent string
 USERAGENT = ('Mozilla/5.0 (iPhone; CPU iPhone OS 12_5_7 like Mac OS X) '
              'AppleWebKit/605.1.15 (KHTML, like Gecko) '
              'Version/12.1.2 Mobile/15E148'
@@ -31,16 +35,36 @@ def response(flow: http.HTTPFlow) -> None:
     '''
     filter responses
     '''
-    # pylint: disable=undefined-variable, too-many-branches
     hostname = flow.request.host
+    uahash = md5sum(flow.request.headers['user-agent'])
     if hostname.endswith(HOSTSUFFIX):
         savefile(
-            os.path.join(FILES, hostname, *flow.request.path_components),
+            os.path.join(
+                FILES, hostname, uahash, *flow.request.path_components),
             flow.response.content
         )
         logging.info('flow.request.path: %s', flow.request.path)
     else:
         logging.info('Not filtering request for %s', flow.request.path)
+
+def md5sum(string, base64encode=True):
+    '''
+    returns md5 hash of (byte)string, urlsafe_b64encoded by default
+
+    >>> md5sum('test')
+    'CY9rzUYh03PK3k6DJie09g=='
+    >>> md5sum('test', False)
+    '098f6bcd4621d373cade4e832627b4f6'
+    '''
+    try:
+        hashed = hashlib.md5(string)
+    except TypeError:
+        hashed = hashlib.md5(string.encode())
+    if base64encode:
+        digest = base64.urlsafe_b64encode(hashed.digest()).decode()
+    else:
+        digest = hashed.hexdigest()
+    return digest
 
 def savefile(path, contents, binary=False, overwrite=False, retry_ok=True):
     '''
