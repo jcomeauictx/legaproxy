85889bda0ef3741ec960b9915aa4603bd207a572
John Comeau <jc@unternet.net>
2024-06-12 06:30:09 -0700

    added operators

--- JavaScript/Python3/jslex.py
+++ JavaScript/Python3/jslex.py
@@ -3,12 +3,20 @@
 fast lexer for javascript
 
 sacrifice completeness for speed
+
+see references JavaScriptLexer.g4 and
+developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar
+
+NOTES:
+    iff="if and only if"
+    nullish="null or undefined"
 '''
 import sys, logging, re  # pylint: disable=multiple-imports
 from string import ascii_letters, digits
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 WHITESPACE = tuple('\t\v\f \xa0\ufeff')  # may need to add more
 ENDLINE = tuple('\n\r\u2028\u2029')
+ZEROWIDTH = tuple('\u200c\u200d')  # non-joiner and joiner
 COMMENTS = {
     '//': ENDLINE,
     '#!': ENDLINE,  # only valid at start of script, but lexer needn't care
@@ -25,14 +33,59 @@ GROUP = {
     '(': ')',
 }
 OPERATOR = [
-    '>>>=',
-    '>>=',
-    '=>',
-    '<<=',
-    '??=',
+    '>>>=',  # right shift logical assign
+    '>>=',   # right shift arithmetic assign
+    '=>',    # arrow
+    '<<=',   # left shift assign
+    '??=',   # nullish coalescing assign
+    '**=',   # power assign
+    '===',   # identity equals
+    '>>>',   # right shift logical
+    '!==',   # identity not equals
+    '...',   # ellipsis ("spread" or "rest" syntax, like Python splat (*))
+    '|=',    # bit-or assign
+    '^=',    # bit-xor assign
+    '*=',    # multiply assign
+    '/=',    # divide assign
+    '%=',    # modulus assign
+    '+=',    # plus assign
+    '-=',    # minus assign
+    '&=',    # bit-and assign
+    '?.',    # optional chaining (access property if LH not nullish)
+    '||',    # logical or
+    '&&',    # logical and
+    '??',    # nullish coalesce (LH becomes RH iff LH is nullish)
+    '++',    # increment
+    '--',    # decrement
+    '>>',    # right shift arithmetic
+    '<<',    # left shift
+    '<=',    # less than assign
+    '>=',    # greater than assign
+    '==',    # equals
+    '!=',    # not equals
+    '**',    # power
+    '?',     # question ("if" as part of ternary operator `a ? b : c`)
+    '|',     # bit-or
+    '^',     # bit-xor
+    '&',     # bit-and
+    '~',     # bit-not
+    '%',     # modulus
+    '#',     # hash (precedes private field identifier in class definition)
+    '>',     # greater than
+    '<',     # less than
+    '*',     # multiply
+    '/',     # divide
+    '+',     # plus
+    '-',     # minus
+    '=',     # assign
+    '.',     # dot (property accessor, `a.b`, also expressed `a["b"]`)
+    '!',     # not
+    ':',     # colon
+    ',',     # comma
+    ';',     # semicolon
 ]
 ID_START = tuple(ascii_letters + '$_')
-ID_CONTINUE = ID_START + tuple(digits)
+ID_CONTINUE = ID_START + tuple(digits) + ZEROWIDTH
 ID = re.compile('[' + ''.join(ID_START) + '][' +
                 ''.join(ID_CONTINUE) + ']*')
 KEYWORDS = [
@@ -79,8 +132,15 @@ def jslex(string):
 
     //developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar
     '''
+    operators = '|'.join([escape(op) for op in OPERATOR])
+    logging.debug('operators: %s', operators)
     tokens = []
-    
+
+def escape(string):
+    '''
+    escape characters for regex
+    '''
+    return string.replace('|', '\|')
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
