606db9f34f0f6325750b54b2913821128c838708
John Comeau <jc@unternet.net>
2026-03-15 06:15:10 -0700

    my first polyfill shim works! now for string.padStart

--- mitm/shims.js
+++ mitm/shims.js
@@ -1,2 +1,17 @@
-if (typeof Element.prototype.append == "undefined")
-	Element.prototype.append = Element.prototype.appendChild;
+/* is it better to use `x.hasOwnProperty(y)` `typeof x.y == "undefined"`? */
+if (typeof Element.prototype.append == "undefined") {
+    Element.prototype.append = function() {
+        var i;
+        for (i = 0; i < arguments.length; i++) {
+            var thing = arguments[i];
+            if (typeof thing == "string") {
+                this.appendChild(document.createTextNode(thing));
+            } else if (thing instanceof Node) {
+                this.appendChild(thing);
+            } else {
+                console.log("cannot appendChild(" + thing + ")");
+            }
+        }
+    };
+}
+/* vim: set tabstop=8 shiftwidth=4 expandtab softtabstop=4: */
